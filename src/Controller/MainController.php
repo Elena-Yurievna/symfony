049c562f30d36eb85461faba77075903c3e0c455
Elena Goncharova <elenagoncharova@Praxiss-MacBook-Pro-2.local>
2024-05-04 10:24:18 +0300

    lesson 1-4

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homePage(): Response
     {
-        return new Response('Hello world!');
+        return $this->render('main/homepage.html.twig');
     }
 }
 
